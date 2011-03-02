8151053d6dbd5919eabfebee3c486fabe98db47d
Wil Alvarez <wil_alejandro@yahoo.com>
2011-03-02 17:39:51 -0430

    Added support to instangr.am y picplz. Added lytebox and generation of html

--- pyparazzi.py
+++ pyparazzi.py
@@ -9,18 +9,25 @@ import urllib2
 import datetime
 from urllib import urlencode
 
-HASHTAG = 'oscars'
-SERVICES = ['plixi.com', 'twitpic.com', 'mobypicture.com']
+# Config variables
+TITLE = 'Galer&iacute;a de prueba'
+COLUMNS = 5
+HASHTAG = ''
+TEMPLATE = 'html/template.html'
+OUTPUT = 'html/test.html'
 
+# Don't touch this :P
+SERVICES = ['plixi.com', 'twitpic.com', 'instagr.am', 'moby.to', 'picplz.com']
 TWITTER_URL = 'http://search.twitter.com/search.json'
-STR_REQ = '%s?q=&ors=twitpic+moby+plixi&tag=%s'
+STR_REQ = '%s?q=&ors=twitpic.com+moby.to+plixi.com+instagr.am+picplz.com&tag=%s&rpp=30'
 URL_PATTERN = re.compile('((http://|ftp://|https://|www\.)[-\w._~:/?#\[\]@!$&\'()*+,;=]*)')
-PLIXI_PATTERN = re.compile('<img (src=\".*?\") (alt=\".*?\") (id=\"photo\")>')
+PLIXI_PATTERN = re.compile('<img (src=\".*?\") (alt=\".*?\") (id=\"photo\") />')
 PLIXI2_PATTERN = re.compile('<img (src=\".*?\") (alt=\".*?\") (style=\".*?\") />')
 TWITPIC_PATTERN = re.compile('<img (class=\"photo\") (id=\"photo-display\") (src=\".*?\") (alt=\".*?\") />')
-MOBY_PATTERN = re.compile('<img (class=\"imageLinkBorder\") (src=\".*?\") (id=\"main_picture\") (alt=\".*?\")>')
+MOBY_PATTERN = re.compile('<img (class=\"imageLinkBorder\") (src=\".*?\") (id=\"main_picture\") (alt=\".*?\") />')
+INSTAGR_PATTERN = re.compile('<img (src=\".*?\") (class=\"photo\") />')
+PICPLZ_PATTERN = re.compile('<img (src=\".*?\") (width=\".*?\") (height=\".*?\") (id=\"mainImage\") (class=\"main-img\") (alt=\".*?\") />')
 
-# TODO picplz e instagram
 def _py26_or_greater():
     import sys
     return sys.hexversion > 0x20600f0
@@ -36,7 +43,6 @@ def detect_urls(text):
     match_urls = URL_PATTERN.findall(text)
     for item in match_urls:
         url = item[0]
-        # Elimina el último paréntesis en las expresiones regulares
         if url[-1] == ')':
             url = url[:-1]
         urls.append(url)
@@ -44,8 +50,6 @@ def detect_urls(text):
     
 def convert_time(str_datetime):
     ''' Take the date/time and convert it into Unix time'''
-    # Tue Mar 13 00:12:41 +0000 2007 -> Tweets normales
-    # Wed, 08 Apr 2009 19:22:10 +0000 -> Busquedas
     month_names = [None, 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
         'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
     
@@ -81,42 +85,40 @@ def get_image_url(url, service):
     comment = ''
     handle = urllib2.urlopen(url)
     response = handle.read()
-    fd = open('/tmp/response', 'w')
-    fd.write(response)
-    fd.close()
     if service == SERVICES[0]:
-        #<img src="http://c0013659.cdn1.cloudfiles.rackspacecloud.com/x2_4c3c8a3" alt="" id="photo">
-        #class="photo"><img src="http://c0013670.cdn1.cloudfiles.rackspacecloud.com/x2_4d4d8c0" alt="" style="max-width:295px" />
         code = PLIXI_PATTERN.findall(response)
         if len(code) <= 0:
             start = response.find('class="photo"><img src')
             end = response.find('>', start + 15) + 1
             piece = response[start:end]
-            print piece
             code = PLIXI2_PATTERN.findall(piece)
         
         if len(code) > 0:
             image_url = code[0][0][5:-1]
-        print len(code), image_url, comment
     elif service == SERVICES[1]:
-        #<img class="photo" id="photo-display" src="http://s3.amazonaws.com/twitpic/photos/full/250705493.jpg?AWSAccessKeyId=0ZRYP5X5F6FSMBCCSE82&amp;Expires=1299078040&amp;Signature=pJAW8i8UkxYMOe%2FvET3kt6oQ8sk%3D" alt="This dude @djspinking just sent me this picture... lmaoooo! this has me rollin right now. shoutout to whoever made this! loll">
         code = TWITPIC_PATTERN.findall(response)
         if len(code) > 0:
             image_url = code[0][2][5:-1]
             comment = code[0][3][5:-1]
-        print len(code), image_url, comment
     elif service == SERVICES[2]:
-        #<img class="imageLinkBorder" src="http://img.mobypicture.com/723dd328b352fb48e1d10e364c59fb7f_view.jpg" id="main_picture" alt="ビックリカメラで目的以外のものを買ってしまった☻写真が横だ。">
+        code = INSTAGR_PATTERN.findall(response)
+        if len(code) > 0:
+            image_url = code[0][0][5:-1]
+    elif service == SERVICES[3]:
         code = MOBY_PATTERN.findall(response)
         if len(code) > 0:
             image_url = code[0][1][5:-1]
             comment = code[0][3][5:-1]
-        print len(code), image_url, comment
+    elif service == SERVICES[4]:
+        code = PICPLZ_PATTERN.findall(response)
+        if len(code) > 0:
+            image_url = code[0][0][5:-1]
+            comment = code[0][5][5:-1]
+    
     return image_url, comment
     
 def get_first_photo(text):
     for url in detect_urls(text):
-        print 'Analyzing %s' % url
         for srv in SERVICES:
             if url.find(srv) > 0:
                 try:
@@ -124,24 +126,53 @@ def get_first_photo(text):
                 except Exception, e:
                     print e
                     return None, None
+    return None, None
     
-results = []
-urlreq = STR_REQ % (TWITTER_URL, HASHTAG)
-handle = urllib2.urlopen(urlreq)
-rtn = handle.read()
-response = json.loads(rtn)
-for tweet in response['results']:
-    user = tweet['from_user']
-    timestamp = convert_time(tweet['created_at'])
-    image_url, comment = get_first_photo(tweet['text'])
-    print image_url
-    if image_url:
-        results.append({
-            'user': user,
-            'timestamp': timestamp,
-            'image_url': image_url,
-            'comment': comment
-        })
+def generate_image(user, timestamp, image_url, comment, first=False):
+    _class = ' first' if first else ''
     
-print results
+    try:
+        return '''<div class="image%s">
+            <a href="%s" rel="lytebox[pyparazzi]" title="%s">
+                <img src="%s" width="150" height="150" />
+            </a>
+            <div class="author">Por: @%s</div>
+            <div class="timestamp">%s</div>
+        </div>''' % (_class, image_url, comment, image_url, user, timestamp)
+    except:
+        return '''<div class="image">
+                <img src="" width="150" height="150" />
+            <div class="author">No se pudo cargar la imagen</div>
+        </div>'''
+    
+    
+def main():
+    urlreq = STR_REQ % (TWITTER_URL, HASHTAG)
+    handle = urllib2.urlopen(urlreq)
+    rtn = handle.read()
+    response = json.loads(rtn)
+    
+    count = 0
+    content = ''
+    for tweet in response['results']:
+        user = tweet['from_user']
+        timestamp = convert_time(tweet['created_at'])
+        image_url, comment = get_first_photo(tweet['text'])
+        if image_url:
+            first = True if (count % COLUMNS == 0) else False
+            content += generate_image(user, timestamp, image_url, comment, first)
+            count += 1
+    
+    fd = open(TEMPLATE, 'r')
+    temp = fd.read()
+    fd.close()
+    
+    page = temp.replace('$title$', TITLE)
+    page = page.replace('$content$', content)
+    
+    fd = open(OUTPUT, 'w')
+    fd.write(page)
+    fd.close()
 
+if __name__ == '__main__':
+    main()
